762177e8afd9b40959e18aab6d44f7548c65071b
Tayoji <595521651@qq.com>
2017-05-21 22:54:03 +0800

    ##

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express')    //加载express模块
 var app = express()
-var port = process.env.PORT || 3000  //监听的端口
+var port = process.env.PORT || 8081  //监听的端口
 app.use(express.static(__dirname))
 app.use(function (req , res){
-  res.sendfile('./')
+  res.sendfile('./html/pay.html')
 })
 
 
